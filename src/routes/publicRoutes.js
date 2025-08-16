745aa3e6a672e0437f19f19b5411c37ed92faf87
ITchanQt <christianmacorol2002@gmail.com>
2025-08-16 23:57:48 +0800

    Add get all users API

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 const { registerAdmin } = require("../controllers/adminController");
 const { registerLaundryShop } = require("../controllers/shopController");
+const { getAllUsers} = require("../controllers/userController")
 
 // Public routes that don't need authentication
 router.post("/register", registerUser);
-router.post("/login", loginUser);
+// router.post("/login", loginUser);
 router.post("/register-admin", registerAdmin);
 router.post("/register-laundry-shop", registerLaundryShop);
+router.get("/users", getAllUsers);
 
 module.exports = router;
