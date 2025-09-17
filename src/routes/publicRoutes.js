8c539bb366abe8744b99a8578aa78f203e25a182
ITchanQt <christianmacorol2002@gmail.com>
2025-09-17 19:01:22 +0800

    Fix user and LS registration, add date registered and fix the login that uses either email or username

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, loginAdmin } = require("../controllers/authController");
-const { registerAdmin } = require("../controllers/adminController");
+    const { registerAdmin } = require("../controllers/adminController");
 const { registerLaundryShop, editShop } = require("../controllers/shopController");
-const { getAllUsers} = require("../controllers/userController")
 
 // Public routes that don't need authentication
 router.post("/register", registerUser);
@@ -11,7 +10,6 @@ router.post("/login", loginUser);
 router.post("/admin/login", loginAdmin);
 router.post("/register-admin", registerAdmin);
 router.post("/register-laundry-shop", registerLaundryShop);
-// router.get("/users", getAllUsers);
 router.put("/edit-shop/:owner_id", editShop);
 
 module.exports = router;
