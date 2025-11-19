10a16375cb2c98a51ac1a114061475cfb030fd79
ITchanQt <christianmacorol2002@gmail.com>
2025-11-19 15:09:00 +0800

    Created API for inserting payment method

--- src/controllers/payment-methods-controller/paymentMethodsContoller.js
+++ src/controllers/payment-methods-controller/paymentMethodsContoller.js
@@ -1,4 +1,5 @@
-const PaymentMethods = require("../../models/payment-methods-model/PaymentMethodModel");
+const { supabase } = require("../../config/supabase");
+const PaymentMethodsModel = require("../../models/payment-methods-model/PaymentMethodModel");
 
 const getAllPaymentMethodsByShopId = async (req, res) => {
   try {
@@ -10,7 +11,9 @@ const getAllPaymentMethodsByShopId = async (req, res) => {
       });
     }
 
-    const paymentMethods = await PaymentMethods.findAllPaymentMethods(shop_id);
+    const paymentMethods = await PaymentMethodsModel.findAllPaymentMethods(
+      shop_id
+    );
     res.status(200).json({
       success: true,
       message: "Payment methods fetch successfully!",
@@ -26,4 +29,86 @@ const getAllPaymentMethodsByShopId = async (req, res) => {
   }
 };
 
-module.exports = { getAllPaymentMethodsByShopId };
+const addPaymentMethod = async (req, res) => {
+  try {
+    const {
+      shop_id,
+      pm_name,
+      account_name,
+      account_number,
+      description,
+      is_displayed,
+      is_static,
+    } = req.body;
+
+    const file = req.file;
+    console.log("FILE:", req.file);
+    if (!file) {
+      return res
+        .status(400)
+        .json({ success: false, message: "QR image is required" });
+    }
+
+    const existingPMname = await PaymentMethodsModel.findByPMName(
+      pm_name,
+      shop_id
+    );
+    if (existingPMname && existingPMname.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Payment Name already exists!",
+      });
+    }
+
+    // Upload to Supabase Storage
+    const fileName = `${shop_id}-${Date.now()}-${file.originalname}`;
+    const { data, error } = await supabase.storage
+      .from("shop-images")
+      .upload(`payment-methods/${fileName}`, file.buffer, {
+        contentType: file.mimetype,
+      });
+
+    if (error) throw error;
+
+    // Get public URL
+    const { data: publicData } = supabase.storage
+      .from("shop-images")
+      .getPublicUrl(`payment-methods/${fileName}`);
+
+    // save record in MySQL
+    const newPaymentMethod = await PaymentMethodsModel.createPaymentMethod({
+      shop_id,
+      pm_name,
+      account_name,
+      account_number,
+      description,
+      is_displayed: is_displayed === "true" ? "true" : "false",
+      is_static,
+      qrCode_image_url: publicData.publicUrl,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Payment method added successfully!",
+      data: {
+        shop_id,
+        pm_name,
+        account_name,
+        account_number,
+        description,
+        is_displayed,
+        is_static,
+        qrCode_image_url: publicData.publicUrl,
+      },
+    });
+  } catch (error) {
+    console.error("addPaymentMethod error:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { getAllPaymentMethodsByShopId, addPaymentMethod };
--- src/models/payment-methods-model/PaymentMethodModel.js
+++ src/models/payment-methods-model/PaymentMethodModel.js
@@ -11,6 +11,58 @@ class PaymentMethods extends BaseModel {
       throw new Error(`Failed to get peyment methods: ${error.message}`);
     }
   }
+
+  static async findByPMName(service_name, shop_id) {
+    try {
+      const sql =
+        "SELECT * FROM payment_method WHERE pm_name = ? AND shop_id = ?";
+      const results = await this.query(sql, [service_name, shop_id]);
+      return results;
+    } catch (error) {
+      console.error(error);
+      throw new Error(error);
+    }
+  }
+
+  static async createPaymentMethod(paymentMethodData) {
+    try {
+      const {
+        shop_id,
+        pm_name,
+        account_name,
+        account_number,
+        description,
+        is_displayed,
+        is_static = "false",
+        qrCode_image_url,
+      } = paymentMethodData;
+      console.log(paymentMethodData)
+      const sql = `INSERT INTO payment_method (
+                    shop_id,
+                    pm_name, 
+                    account_name, 
+                    account_number, 
+                    description, 
+                    is_displayed, 
+                    is_static, 
+                    qrCode_image_url)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+
+      return this.query(sql, [
+        shop_id,
+        pm_name,
+        account_name,
+        account_number,
+        description,
+        is_displayed,
+        is_static,
+        qrCode_image_url,
+      ]);
+    } catch (error) {
+      console.error("Error creating shop payment method:", error);
+      throw new Error(`Failed to create shop payment method: ${error.message}`);
+    }
+  }
 }
 
 module.exports = PaymentMethods;
